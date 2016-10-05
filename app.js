2a276417793b66638b2621d4aa4a6f78f7e3e096
Jonathan Elias Caicedo <Jonathan@jcaicedo.tk>
2016-10-05 16:57:18 -0400

    Add more needed dependencies and initialize app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+//dependencies
+const express = require('express');
+const mongoose = require('mongoose');
+const body-parser = require('body-parser');
+const cookie-parser = require('cookie-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
