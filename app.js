f2477a1345558941fa4368822cbf31bc664ebdaf
Jonathan Elias Caicedo <Jonathan@jcaicedo.tk>
2016-10-06 13:00:30 -0400

    Add cookie-parser and fix dependencies in app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //dependencies
 const express = require('express');
 const mongoose = require('mongoose');
-const body-parser = require('body-parser');
-const cookie-parser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
