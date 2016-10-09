4dcecef73c614fd4ff10b530467f03a818dae018
jcbird1012 <jbirdcaicedo@yahoo.com>
2016-10-09 20:47:14 +0000

    Ensure favicon is served correctly.

--- app.js
+++ app.js
@@ -2,16 +2,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const favicon = require('serve-favicon');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
-const path = 'path';
+const path = require('path');
 const app = express();
 
 app.use(express.static('web'));
 app.use('/scripts', express.static('node_modules'));
 app.use('/app', express.static('web/app'));
+app.use(favicon(path.join(__dirname, '/web/assets/images', 'favicon.ico')));
 
 app.listen(3000, function () {
-  console.log('listening on port 3000');
+  console.log('Listening on port 3000.');
 });
