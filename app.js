d9200b5d6c7d454b3ab44cdd791a833d7e1d06aa
jcbird1012 <jbirdcaicedo@yahoo.com>
2016-10-09 20:33:42 +0000

    Add Angular 2 Quickstart code, and prepare for development.

--- app.js
+++ app.js
@@ -5,3 +5,13 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
+const path = 'path';
+const app = express();
+
+app.use(express.static('web'));
+app.use('/scripts', express.static('node_modules'));
+app.use('/app', express.static('web/app'));
+
+app.listen(3000, function () {
+  console.log('listening on port 3000');
+});
