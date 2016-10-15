e3b5bed08e51c0a2b4a2b6d4946af35756c2abb4
jcbird1012 <jbirdcaicedo@yahoo.com>
2016-10-15 22:50:13 +0000

    Redirect automatically when user is logged in, add CMS properly.

--- app.js
+++ app.js
@@ -6,16 +6,15 @@ const favicon = require('serve-favicon');
 const session = require('express-session');
 const NedbStore = require('connect-nedb-session')(session);
 const path = require('path');
-var config = require('./config.js');
-var cms = require('./cms.js');
+const config = require('./config.js');
+const cms = require('./cms.js');
 const app = express();
 
-app.use(express.static('web'));
 app.use('/scripts', express.static('node_modules'));
 app.use('/app', express.static('web/app'));
 app.use(favicon(path.join(__dirname, '/web/assets/images', 'favicon.ico')));
 
-var sessionStore = new NedbStore({ filename: config.session_persistence_file });
+const sessionStore = new NedbStore({ filename: config.session_persistence_file });
 
 app.use(session({
     secret: process.env.SESSION_SECRET || 'super secret key',
@@ -25,22 +24,44 @@ app.use(session({
 }));
 
 // Create a new instance of CASAuthentication.
-var cas = new CASAuthentication({
+const cas = new CASAuthentication({
     cas_url: 'https://cas-auth.rpi.edu/cas',
     service_url: config.service_url,
     cas_version: '2.0',
 });
 
+//ROUTES
+
+app.get('/', function (req, res, next) {
+   if (req.session.cas_user) {
+      res.redirect('/dashboard');
+   }
+   
+   else {
+      app.use(express.static('web'));
+      next();
+   }
+});
+
 app.get('/login', cas.bounce, function (req, res) {
    if (!req.session || !req.session.cas_user) {
         res.redirect('/logout');
-    }
+   }
    
-   
+   res.redirect('/dashboard');
+});
+
+app.get('/dashboard', function (req, res) {
+   if (!req.session || !req.session.cas_user) {
+        res.redirect('/login');
+   }
    var rcs_id = req.session.cas_user.toLowerCase();
-   console.log(cms.getRCS(rcs_id));
-   res.send( '<html><body><a href = "/logout">Hello ' + rcs_id + '!</a></body></html>' );
-   console.log(rcs_id);
+    cms.getRCS(rcs_id).then(function (user_data) {
+       user_data = JSON.parse(user_data);
+       var first_name = user_data.preferred_name || user_data.first_name;
+       // username, student_id (is rin), last_name, middle_name, user_type
+       res.send( '<html><body><a href = "/logout">Hello ' + first_name + '!</a></body></html>' );
+   });
 });
 
 app.get('/logout', cas.logout);
