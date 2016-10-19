b3ad80402fc7719102f09ca9325ba944cbe3890c
jcbird1012 <jbirdcaicedo@yahoo.com>
2016-10-19 19:39:10 +0000

    Fix spacing for dashboard loading screen.

--- web/assets/js/dashboard.js
+++ web/assets/js/dashboard.js
@@ -1,7 +1,7 @@
 // Add name to loading screen.
 $.getJSON( "/api/current_user", function( data ) {
    var name = data.first_name;
-   $(".loading").append('<h1>Hello there ' + name + ' !</h1>')
+   $(".loading").append('<h1>Hello there ' + name + '!</h1>')
    $(".loading").append("<h4 class = 'loading-text'>We're setting things up for you</h4>");
 });
 
