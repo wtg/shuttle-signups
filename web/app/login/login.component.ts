e85c62908a88610df2202907f723433997e8ff7d
lucienbrule <lucien@lucienbrule.com>
2016-11-29 22:20:39 -0500

    Fix angular components

--- web/app/login/login.component.ts
+++ web/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 @Component({
-  selector: 'shuttle-signups',
+  selector: 'shuttle-login',
   templateUrl: 'views/partials/login.html'
 })
 export class LoginComponent { }
--- web/assets/js/dashboard.js
+++ web/assets/js/dashboard.js
@@ -1,7 +1,11 @@
 // Add name to loading screen.
-$.getJSON( "/api/current_user", function( data ) {
-   var name = data.first_name;
-   $(".loading").append('<h1>Hello there ' + name + '!</h1>')
-   $(".loading").append("<h4 class = 'loading-text'>We're setting things up for you</h4>");
+$.getJSON("/api/current_user", function(data) {
+    var name = data.first_name;
+    $(".loading").append('<h1>Hello there ' + name + '!</h1>')
+    $(".loading").append("<h4 class = 'loading-text'>We're setting things up for you</h4>");
+}).done(function() {
+    // make it look like it's loading / give everythign a chance to populate
+    setTimeout(function() {
+        $("shuttle-login").remove();
+    }, 750);
 });
-
