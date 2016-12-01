8493ce62d3cedfb777951541343a21e3d055b52d
lucienbrule <lucien@lucienbrule.com>
2016-12-01 14:46:16 -0500

    add current user object load from api route

--- web/app/dashboard/dashboard.component.ts
+++ web/app/dashboard/dashboard.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
+import { Observable }       from 'rxjs/Observable';
 import { DashboardService } from './dashboard.service';
 import {User} from './user';
 @Component({
     selector: 'shuttle-dashboard',
-    templateUrl: 'views/partials/dashboard.html'
+    templateUrl: 'views/partials/dashboard.html',
+    providers: [DashboardService]
 })
 
 export class DashboardComponent {
   public user: User;
-  constructor(){
-    this.user = new User("Lucien","brulel");
+  public data: any;
+  public d_user: any;
+  public d_error: any;
+  constructor(private dashboardService: DashboardService){
+    this.user = new User("Shirley","001RPI");
+    this.dashboardService.getUser().then(user =>this.user = user);
+    this.data = this.dashboardService.getUser();
+    console.log("Made a Component");
   }
 }
--- web/app/dashboard/dashboard.module.ts
+++ web/app/dashboard/dashboard.module.ts
@@ -1,9 +1,13 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { DashboardComponent }   from './dashboard.component';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+// import { FormsModule } from '@angular/forms';
+import { HttpModule, JsonpModule } from '@angular/http';
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ DashboardComponent ],
-  bootstrap:    [ DashboardComponent ]
+    imports: [BrowserModule, HttpModule, JsonpModule],
+    declarations: [DashboardComponent],
+    bootstrap: [DashboardComponent]
 })
 export class DashboardModule { }
+platformBrowserDynamic().bootstrapModule(DashboardModule);
