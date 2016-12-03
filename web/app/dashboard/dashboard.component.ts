872c0871c88f0e78294b248f4bfa806ee21cd397
lucienbrule <lucien@lucienbrule.com>
2016-12-02 23:07:12 -0500

    remove duplicated shuttles from UI

--- web/app/dashboard/dashboard.component.ts
+++ web/app/dashboard/dashboard.component.ts
@@ -16,22 +16,18 @@ export class DashboardComponent implements OnInit {
     public user: User;
     public shuttles: Shuttle[];
     public usershuttles: Shuttle[];
-    private godmode:boolean;
+    private godmode: boolean;
     //dashboard constructor, fetch data from server
     constructor(private dashboardService: DashboardService) {
-      this.godmode = false;
+        this.godmode = false;
         // Display entertaining blank user while async load
         this.user = new User();
-        // this.shuttles = new Array<Shuttle>();
 
         //async load current user from /current_user
-        this.dashboardService.getUser().then(user => this.user = new User(user));
+        this.getuser();
+
         //async load list of shuttles from /get_shuttles
-        this.dashboardService.getShuttles().then(shuttles => {
-            console.log(this.shuttles = shuttles);
-            this.dashboardService.getusershuttles(this.user).then(data => this.usershuttles = data
-            )
-        });
+        this.getshuttles();
 
         //debug log that the component loaded.
         console.log("Made a Component");
@@ -41,37 +37,72 @@ export class DashboardComponent implements OnInit {
     ngOnInit() {
         console.log("called init");
     }
+    getuser() {
+        this.dashboardService.getUser().then(user => this.user = new User(user));
+    }
+    getshuttles() {
+        this.dashboardService.getShuttles().then(shuttles => {
+            console.log(this.shuttles = shuttles);
+            this.dashboardService.getusershuttles(this.user).then(data => {
+                this.usershuttles = data;
+                //now diff user shuttles and availible shuttles
+                console.log("diff:");
+                var s = new Map();
+                var s_buf = [];
+                for(var i = 0;i < this.usershuttles.length;i++){
+                  s.set(this.usershuttles[i]._id,this.usershuttles[i]);
+                }
+                console.log(s);
+                for(var i = 0; i < this.shuttles.length;i++){
+                  console.log(this.shuttles[i]._id,s.has(this.shuttles[i]._id));
+                  if(! s.has(this.shuttles[i]._id)){
+                    s_buf.push(this.shuttles[i]);
+                  }
+                }
+                this.shuttles=s_buf;
+                console.log(s_buf);
+                // console.log(this.shuttles.filter(shuttle => this.usershuttles.indexOf(shuttle, 0) < 0));
+
+            }
+            )
+        });
+    }
     signup(shuttle: Shuttle) {
         this.dashboardService.signup(this.user, shuttle).then(data => {
-          this.usershuttles.push(shuttle);
-          var index: number = this.shuttles.indexOf(shuttle,0);
-          if(index > -1){
-            this.shuttles.splice(index,1);
-          }
+            this.usershuttles.push(shuttle);
+            var index: number = this.shuttles.indexOf(shuttle, 0);
+            if (index > -1) {
+                this.shuttles.splice(index, 1);
+            }
         }
         )
     }
     unsignup(shuttle: Shuttle) {
-        var res: any = this.dashboardService.unsignup(this.user, shuttle).then(data => this.usershuttles.pop());
-        res.then(d => console.log("d is" + d,d));
+        this.dashboardService.unsignup(this.user, shuttle).then(data => {
+            this.shuttles.push(shuttle);
+            var index: number = this.usershuttles.indexOf(shuttle, 0);
+            if (index > -1) {
+                this.usershuttles.splice(index, 1);
+            }
+        });
     }
     getusershuttles() {
         this.dashboardService.getusershuttles(this.user).then(shuttles =>
-          console.log(this.usershuttles = shuttles));
+            console.log(this.usershuttles = shuttles));
     }
-    togglegodmode(){
-      this.godmode = !this.godmode;
-      // console.log("godmode: " + this.godmode)
+    togglegodmode() {
+        this.godmode = !this.godmode;
+        // console.log("godmode: " + this.godmode)
     }
-    deleteshuttle(shuttle:Shuttle){
-      this.dashboardService.deleteshuttle(shuttle);
+    deleteshuttle(shuttle: Shuttle) {
+        this.dashboardService.deleteshuttle(shuttle);
     }
-    cancelshuttle(shuttle:Shuttle){
-      this.dashboardService.cancelshuttle(shuttle);
+    cancelshuttle(shuttle: Shuttle) {
+        this.dashboardService.cancelshuttle(shuttle);
 
     }
-    addshuttle(shuttle:Shuttle){
-      this.dashboardService.addshuttle(shuttle);
+    addshuttle(shuttle: Shuttle) {
+        this.dashboardService.addshuttle(shuttle);
     }
     // modifyshuttle(shuttle:Shuttle){
     //   this.dashboardService.modifyshuttle(shuttle);
