2a7850842f9fcbb197879b2a8e53289a989081f1
lucienbrule <lucien@lucienbrule.com>
2017-03-04 20:49:40 -0500

    flex layout all the things

--- web/app/dashboard/user.ts
+++ web/app/dashboard/user.ts
@@ -1,16 +1,20 @@
 import {Shuttle} from './shuttle'
 export class User {
   first_name: string;
+  last_name:string;
   username:string;
   numGuests:number;
   guestsOnly:boolean;
   shuttles:Array<Shuttle>;
+  isAdmin: boolean;
   constructor(arg?:any){
     console.log("User created.");
     this.first_name = arg && arg.first_name;
+    this.last_name = arg && arg.last_name;
     this.username = arg && arg.username;
     this.numGuests = arg && arg.numGuests;
     this.guestsOnly = arg && arg.guestsOnly;
     this.shuttles = Array<Shuttle>();
+    this.isAdmin = arg && arg.isAdmin;
   }
 }
