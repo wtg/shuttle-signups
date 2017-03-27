6826a42a3870dbcfacc86885d92c12b163f97aab
Jonathan Elias Caicedo <Jonathan@jcaicedo.tk>
2017-03-27 02:20:25 +0000

    Make font bold on login page; change @ to at in credits

--- web/app/dashboard/dashboard.ts
+++ web/app/dashboard/dashboard.ts
@@ -1,3 +1,5 @@
+import {enableProdMode} from "@angular/core";
+enableProdMode();
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { DashboardModule } from './dashboard.module';
 const platform = platformBrowserDynamic();
