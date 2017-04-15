0681a28aeda4ea5e8851b06f3c96570ca9a94fd8
Jonathan Elias Caicedo <Jonathan@jcaicedo.tk>
2017-04-15 17:33:00 +0000

    Update syntax in unsignup-shuttle

--- routes/unsignup-shuttle.js
+++ routes/unsignup-shuttle.js
@@ -16,7 +16,7 @@ router.post('/', function(req, res) {
 	var shuttleID = req.body.id;
 	var numGuests = req.body.numGuests;
 	var guestsOnly = req.body.guestsOnly;
-	if (typeof numGuests != "number" || typeof guestsOnly != "boolean") {
+	if (typeof numGuests !== "number" || typeof guestsOnly !== "boolean") {
 		res.status(400);
 		res.send("Your request appears to be malformed.");
 		return;
@@ -38,7 +38,7 @@ router.post('/', function(req, res) {
 		var shuttle = docs[0];
 		// Mongoose doesn't always throw an error if given an ID for a shuttle that doesn't exist
 		// This if-conditional fixes that
-		if (shuttle == null) {
+		if (shuttle === null) {
 			res.send("That shuttle doesn't exist.");
 			return;
 		}
@@ -50,7 +50,7 @@ router.post('/', function(req, res) {
 			return;
 		}
 		// Now, let's check to see if the user is even signed up for this shuttle
-		if (riders.indexOf(rcs_id) == -1 && waitlist.indexOf(rcs_id) == -1) {
+		if (riders.indexOf(rcs_id) === -1 && waitlist.indexOf(rcs_id) === -1) {
 			res.send("You're not signed up for shuttle " + shuttleID);
 			return;
 		}
@@ -59,7 +59,7 @@ router.post('/', function(req, res) {
 		
 		if (!guestsOnly) {
 			// Let's figure out if they're on the waitlist or if they're on the riders list
-			if (riders.indexOf(rcs_id) != -1) {
+			if (riders.indexOf(rcs_id) !== -1) {
 				for (var user = 0; user < riders.length; ++user) {
 					if (riders[user].includes(rcs_id)) {
 						riders.splice(user, 1);
@@ -102,7 +102,7 @@ router.post('/', function(req, res) {
 		else {
 			var guestsRemoved = 0;
 			// Let's figure out if the guests are on the waitlist or the riders list
-			if (riders.indexOf(rcs_id) != -1) {
+			if (riders.indexOf(rcs_id) !== -1) {
 				for (var i = 0; i < numGuests; ++i) {
 					for (var user = 0; user < riders.length; ++user) {
 						if (riders[user].includes(rcs_id + "-guest")) {
